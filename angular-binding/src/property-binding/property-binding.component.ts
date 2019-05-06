3f668a046986577600e86005389a55d42fc1cef2
Ranjith Sekar <ranjith.sekar@clarivate.com>
2019-05-06 21:15:58 +0530

    Binding Example

---
+++ angular-binding/src/property-binding/property-binding.component.ts
@@ -0,0 +1,54 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'property-binding',
+  template: `<h4>Property Binding:</h4>
+              First Name: {{ firstname }} <br>
+              Last Name: {{ lastname }} <br>
+              Age: <input type='text' [value]='age'> <br>
+              About Me: <p [innerHtml]='aboutme'></p>
+              <button bind-disabled='login'>Login</button> <br><br>
+              <button [disabled]='reset'>Reset</button><br><br>
+              Message: {{message}}`,
+  styles: [`h1 { font-family: Lato; }`]
+})
+export class PropertyBindingComponent {
+  firstname = 'Roger';
+  lastname = "Federer";
+  age = 30;
+  aboutme = "I am a World class Tennis Player";
+  login: boolean = false;
+  reset: boolean = true;
+  message: any;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**template: `First Name: {{ firstname }} <br>
+              Last Name: {{ lastname }} <br>
+              Age: <input type='text' [value]='age'> <br>
+              About Me: <p [innerHtml]='aboutme'></p>
+              <button bind-disabled='login' >Login</button> <br><br>
+              <button bind-disabled='reset' >Reset</button>` */
